60eef39a07ffb6cfa17cf0c5248f097e6bc732b0
Lucasedusi <lucasmto.16@hotmail.com>
2021-07-15 15:41:36 -0300

    add cors

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://www.receitaws.com.br',
+  baseURL: 'https://cors-anywhere.herokuapp.com/receitaws.com.br/',
 });
 
 export default api;
