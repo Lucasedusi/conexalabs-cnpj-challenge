c9a698c406e86d446f870e4a8f28629808a10f28
Lucasedusi <lucasmto.16@hotmail.com>
2021-07-15 11:34:18 -0300

    transpiling mapbox

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,10 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            ignore: ['./node_modules/mapbox-gl/dist/mapbox-gl.js'],
+          },
         },
       },
     ],
