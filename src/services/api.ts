f0d81d2e813c27eb5ec7829b0082db1cdb6e0148
Lucas Eduardo <33089713+Lucasedusi@users.noreply.github.com>
2021-10-20 16:42:03 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Foi utilizado https://cors.bridged.cc antes da api original oara configuração do CORS
 
 const api = axios.create({
-  baseURL: 'https://cors.bridged.cc/https://receitaws.com.br',
+  baseURL: 'https://cors-anywhere.herokuapp.com/https://receitaws.com.br',
 });
 
 export default api;
